9c6e5d3f6d6107e4306b3ba62d5693810fe41b33
sureshnath <sureshnath@hotmail.com>
2017-11-27 00:15:39 +0000

    dao and test added

---
+++ ie2e-t4-suresh/src/main/java/com/ie2e/t4/repository/ConfigurableFileInfoDao.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ie2e.t4.repository;
+
+import com.ie2e.t4.model.FileInfo;
+import com.ie2e.t4.util.FileNotDirNamePatternFilter;
+import java.io.File;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Suresh
+ */
+@Repository
+public class ConfigurableFileInfoDao implements IFileInfoDao {
+
+    private static final Logger LOG = Logger.getLogger(ConfigurableFileInfoDao.class);
+    public static final String SYS_PROP_CONF_DIR = "com.ie2e.t4.conf.dir";
+
+    @Autowired
+    private Environment env;
+
+    public Environment getEnv() {
+        return env;
+    }
+
+    public void setEnv(Environment env) {
+        this.env = env;
+    }
+
+    private List<FileInfo> listFileInfos(FileNotDirNamePatternFilter filter) {
+        List<FileInfo> fileInfos = new ArrayList<>();
+        String configDirPath = env.getProperty(SYS_PROP_CONF_DIR);
+        File configuredDirectory = new File(configDirPath==null?"":configDirPath);
+        if (configuredDirectory.exists()) {
+            for (File file : configuredDirectory.listFiles(filter)) {
+                FileInfo fileInfo = new FileInfo();
+                fileInfo.setName(file.getName());
+                fileInfo.setSize(file.length());
+                fileInfo.setExtension(FilenameUtils.getExtension(file.getName()));
+                fileInfo.setMimeType(URLConnection.guessContentTypeFromName(file.getName()));
+                fileInfos.add(fileInfo);
+            }
+        } else {
+            LOG.warn("DAO configured to use directory, but does NOT exists: " + configuredDirectory.getAbsolutePath());
+        }
+        return fileInfos;
+    }
+
+    @Override
+    public List<FileInfo> findAll() {
+       return listFileInfos(FileNotDirNamePatternFilter.getAllFilesMatcher());
+    }
+
+    @Override
+    public List<FileInfo> findMimeExcel() {
+       return listFileInfos(FileNotDirNamePatternFilter.getExcelFilesMatcher());
+    }
+
+    @Override
+    public List<FileInfo> findMimeCsv() {
+       return listFileInfos(FileNotDirNamePatternFilter.getCsvFilesMatcher());
+    }
+
+}
---
+++ ie2e-t4-suresh/src/test/java/com/ie2e/t4/repository/ConfigurableFileInfoDaoTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ie2e.t4.repository;
+
+import com.ie2e.t4.model.FileInfo;
+import com.ie2e.t4.util.TestConfig;
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ *
+ * @author Suresh
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/spring-beans.xml")
+public class ConfigurableFileInfoDaoTest {
+    
+    @Autowired
+    private IFileInfoDao instance;
+    
+    @Before
+    public void setupEnvVar(){
+        System.setProperty(ConfigurableFileInfoDao.SYS_PROP_CONF_DIR, TestConfig.FILE_NAME_FILTER_TEST_FOLDER);
+    }
+
+    @Test
+    public void testFindAll() {
+        List<FileInfo> result = instance.findAll();
+        assertEquals(4, result.size());
+    }
+
+    @Test
+    public void testFindMimeExcel() {
+        List<FileInfo> result = instance.findMimeExcel();
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    public void testFindMimeCsv() {
+        List<FileInfo> result = instance.findMimeCsv();
+        assertEquals(1, result.size());
+    }
+
+}
